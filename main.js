c6320be3b6967058c1007a4e49e1fb7dc0b625f5
Garrett Troy <gt0840922@otc.edu>
2017-10-02 13:10:54 -0500

    I got it to work

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let button = 
+let btn = document.getElementById("like");
 
 btn.addEventListener("click", e=>{
 let currentCount = btn.nextSibling.textContent.trim()
 newCount = parseInt(currentCount)+1
-})
+btn.nextSibling.textContent = newCount
+});
